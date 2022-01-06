f378e7ae80b94e406e27117796d2a9b2efb8f34a
Khairul38 <khairulislam38@gmail.com>
2022-01-06 20:38:57 +0600

    update

--- script/app.js
+++ script/app.js
@@ -48,46 +48,96 @@ const drawWhenScroll = () => {
         dotsfill5.style.opacity = 0;
         dotsfill6.style.opacity = 0;
         dotsfill7.style.opacity = 0;
-        img11.classList.add("img22A")
-        img12.classList.add("animate__fadeInUp")
-        img13.classList.add("animate__fadeInDown")
-        img14.classList.add("animate__fadeInDown")
-        img15.classList.add("animate__fadeInUp")
 
-        if (img21.classList.contains("animate__fadeInUp")) {
-            img21.classList.remove("animate__fadeInUp")
+        img41.classList.add("img41A")
+        img42.classList.add("img42A")
+
+        if (img51.classList.contains("animate__fadeInDown")) {
+            img51.classList.remove("animate__fadeInDown")
         }
-        if (img22.classList.contains("animate__fadeInDown")) {
-            img22.classList.remove("animate__fadeInDown")
+        if (img52.classList.contains("animate__fadeInUp")) {
+            img52.classList.remove("animate__fadeInUp")
+        }
+        if (img53.classList.contains("animate__fadeInDown")) {
+            img53.classList.remove("animate__fadeInDown")
+        }
+        if (img54.classList.contains("animate__fadeInUp")) {
+            img54.classList.remove("animate__fadeInUp")
+        }
+        if (img55.classList.contains("img55A")) {
+            img55.classList.remove("img55A")
         }
 
-        firstContent.style.backgroundColor = 'rgb(34, 47, 66)'
-        svgPath.style.backgroundColor = 'rgb(34, 47, 66)'
+        firstContent.style.backgroundColor = '#5c298b'
+        svgPath.style.backgroundColor = '#5c298b'
         svgSec.innerHTML = `
-        <div class=" text-light align-div1-text">
-        <h5 class='animate__animated animate__fadeInUp'>Redefining</h5>
-        <h1 class='animate__animated animate__fadeInUp'>UX Strategy</h1>
-        <h5 class="contentBottom text-end">and UI design</h5>
+        <div class=" text-light align-div2-text">
+        <h1 class="animate__animated animate__fadeInUp">25M+ Downloads</h1>
+        <h5 class="animate__animated animate__fadeInDown text-end">on appstore & google playstore</h5>
         </div>`;
         contentSec.innerHTML = `
         <div class=''>
-        <h1 class='animate__animated animate__fadeInDown'>ABC 456</h1>
-        <div class="animate__animated animate__fadeIn">
-        <span>We are the best</span><br>
-        <span>web development company</span><br>
-        <span>in the world</span>
+        <img width='200px' class='mb-3' src="/ScrollAnimation Task Assets/world-communication-awards-for-best-digital-experience.png" alt="">
+        <h1 class='animate__animated animate__fadeInDown'>ABC 235</h1>
+        <div class='animate__animated animate__fadeIn'>
+        <span>We are the best web development</span><br>
+        <span> company in the world</span>
         </div>
         <br>
-        <div class="d-flex justify-content-between">
-            <p class="animate__animated animate__zoomIn">Vew cose study <i class="fas fa-arrow-right"></i></p>
+        <div class="mt-4 d-flex justify-content-between">
+            <p class='animate__animated animate__zoomIn'>View case study <i class="fas fa-arrow-right"></i></p>
             <p class="me-5">SKIP</p>
         </div></div>`;
+        img31.classList.remove("animate__fadeInUp")
+        img32.classList.remove("animate__fadeInDown")
     }
     // 2nd div
     else if (drawPath == 679.8452758789062) {
         dotsfill1.style.opacity = 1;
         dotsfill2.style.opacity = 1;
         dotsfill3.style.opacity = 0;
+        dotsfill4.style.opacity = 0;
+        dotsfill5.style.opacity = 0;
+        dotsfill6.style.opacity = 0;
+        dotsfill7.style.opacity = 0;
+
+        img31.classList.add("animate__fadeInUp")
+        img32.classList.add("animate__fadeInDown")
+        if (img41.classList.contains("img41A")) {
+            img41.classList.remove("img41A")
+        }
+        if (img42.classList.contains("img42A")) {
+            img42.classList.remove("img42A")
+        }
+
+        firstContent.style.backgroundColor = '#393299'
+        svgPath.style.backgroundColor = '#393299'
+        svgSec.innerHTML = `
+        <div class=" text-light align-div1-text">
+        <h5 class="animate__animated animate__fadeInDown">The Next Big</h5>
+        <h1 class="animate__animated animate__fadeInDown">Blockchain</h1>
+        <h5 class="contentBottom text-end">Revolution</h5>
+        </div>`;
+        contentSec.innerHTML = `
+        <div>
+        <h1 class='animate__animated animate__fadeInUp'>ABC 234</h1>
+        <div class='animate__animated animate__fadeIn'>
+        <span>We are the best web development</span><br>
+        <span> company in the world</span>
+        </div>
+        <br>
+        <div class="mt-4 d-flex justify-content-between">
+            <p class='svgtext1 animate__animated animate__zoomIn'>coming soon</p>
+            <p class="me-5">SKIP</p>
+        </div></div>`;
+        img21.classList.remove("animate__fadeInUp")
+        img22.classList.remove("animate__fadeInDown")
+    }
+    // 3rd div
+    else if (drawPath == 543.876220703125) {
+        dotsfill1.style.opacity = 1;
+        dotsfill2.style.opacity = 1;
+        dotsfill3.style.opacity = 1;
         dotsfill4.style.opacity = 0;
         dotsfill5.style.opacity = 0;
         dotsfill6.style.opacity = 0;
@@ -130,48 +180,6 @@ const drawWhenScroll = () => {
         img14.classList.remove("animate__fadeInDown")
         img15.classList.remove("animate__fadeInUp")
     }
-    // 3rd div
-    else if (drawPath == 543.876220703125) {
-        dotsfill1.style.opacity = 1;
-        dotsfill2.style.opacity = 1;
-        dotsfill3.style.opacity = 1;
-        dotsfill4.style.opacity = 0;
-        dotsfill5.style.opacity = 0;
-        dotsfill6.style.opacity = 0;
-        dotsfill7.style.opacity = 0;
-
-        img31.classList.add("animate__fadeInUp")
-        img32.classList.add("animate__fadeInDown")
-        if (img41.classList.contains("img41A")) {
-            img41.classList.remove("img41A")
-        }
-        if (img42.classList.contains("img42A")) {
-            img42.classList.remove("img42A")
-        }
-
-        firstContent.style.backgroundColor = '#393299'
-        svgPath.style.backgroundColor = '#393299'
-        svgSec.innerHTML = `
-        <div class=" text-light align-div1-text">
-        <h5 class="animate__animated animate__fadeInDown">The Next Big</h5>
-        <h1 class="animate__animated animate__fadeInDown">Blockchain</h1>
-        <h5 class="contentBottom text-end">Revolution</h5>
-        </div>`;
-        contentSec.innerHTML = `
-        <div>
-        <h1 class='animate__animated animate__fadeInUp'>ABC 235</h1>
-        <div class='animate__animated animate__fadeIn'>
-        <span>We are the best web development</span><br>
-        <span> company in the world</span>
-        </div>
-        <br>
-        <div class="mt-4 d-flex justify-content-between">
-            <p class='svgtext1 animate__animated animate__zoomIn'>coming soon</p>
-            <p class="me-5">SKIP</p>
-        </div></div>`;
-        img21.classList.remove("animate__fadeInUp")
-        img22.classList.remove("animate__fadeInDown")
-    }
     // 4th div
     else if (drawPath == 407.90716552734375) {
         dotsfill1.style.opacity = 1;
@@ -181,48 +189,40 @@ const drawWhenScroll = () => {
         dotsfill5.style.opacity = 0;
         dotsfill6.style.opacity = 0;
         dotsfill7.style.opacity = 0;
+        img11.classList.add("img22A")
+        img12.classList.add("animate__fadeInUp")
+        img13.classList.add("animate__fadeInDown")
+        img14.classList.add("animate__fadeInDown")
+        img15.classList.add("animate__fadeInUp")
 
-        img41.classList.add("img41A")
-        img42.classList.add("img42A")
-
-        if (img51.classList.contains("animate__fadeInDown")) {
-            img51.classList.remove("animate__fadeInDown")
+        if (img21.classList.contains("animate__fadeInUp")) {
+            img21.classList.remove("animate__fadeInUp")
         }
-        if (img52.classList.contains("animate__fadeInUp")) {
-            img52.classList.remove("animate__fadeInUp")
-        }
-        if (img53.classList.contains("animate__fadeInDown")) {
-            img53.classList.remove("animate__fadeInDown")
-        }
-        if (img54.classList.contains("animate__fadeInUp")) {
-            img54.classList.remove("animate__fadeInUp")
-        }
-        if (img55.classList.contains("img55A")) {
-            img55.classList.remove("img55A")
+        if (img22.classList.contains("animate__fadeInDown")) {
+            img22.classList.remove("animate__fadeInDown")
         }
 
-        firstContent.style.backgroundColor = '#5c298b'
-        svgPath.style.backgroundColor = '#5c298b'
+        firstContent.style.backgroundColor = 'rgb(34, 47, 66)'
+        svgPath.style.backgroundColor = 'rgb(34, 47, 66)'
         svgSec.innerHTML = `
-        <div class=" text-light align-div2-text">
-        <h1 class="animate__animated animate__fadeInUp">25M+ Downloads</h1>
-        <h5 class="animate__animated animate__fadeInDown text-end">on appstore & googlr playstore</h5>
+        <div class=" text-light align-div1-text">
+        <h5 class='animate__animated animate__fadeInUp'>Redefining</h5>
+        <h1 class='animate__animated animate__fadeInUp'>UX Strategy</h1>
+        <h5 class="contentBottom text-end">and UI design</h5>
         </div>`;
         contentSec.innerHTML = `
         <div class=''>
-        <img width='200px' class='mb-3' src="/ScrollAnimation Task Assets/world-communication-awards-for-best-digital-experience.png" alt="">
-        <h1 class='animate__animated animate__fadeInDown'>ABC 235</h1>
-        <div class='animate__animated animate__fadeIn'>
-        <span>We are the best web development</span><br>
-        <span> company in the world</span>
+        <h1 class='animate__animated animate__fadeInDown'>ABC 456</h1>
+        <div class="animate__animated animate__fadeIn">
+        <span>We are the best</span><br>
+        <span>web development company</span><br>
+        <span>in the world</span>
         </div>
         <br>
-        <div class="mt-4 d-flex justify-content-between">
-            <p class='animate__animated animate__zoomIn'>Vew cose study <i class="fas fa-arrow-right"></i></p>
+        <div class="d-flex justify-content-between">
+            <p class="animate__animated animate__zoomIn">Vew cose study <i class="fas fa-arrow-right"></i></p>
             <p class="me-5">SKIP</p>
         </div></div>`;
-        img31.classList.remove("animate__fadeInUp")
-        img32.classList.remove("animate__fadeInDown")
     }
     // 5th div
     else if (drawPath == 271.9381103515625) {
@@ -291,13 +291,13 @@ const drawWhenScroll = () => {
         <div class=''>
         <h1 class='animate__animated animate__fadeInUp'>ABC 678</h1>
         <div class="animate__animated animate__fadeIn">
-        <span>Best since 2007</span><br>
+        <span>Best since 2017</span><br>
         <span>We offer wide range of</span><br>
         <span>web and app developing</span><br>
         </div>
         <br>
         <div class="mt-4 d-flex justify-content-between">
-            <p class='animate__animated animate__zoomIn'>Vew cose study <i class="fas fa-arrow-right"></i></p>
+            <p class='animate__animated animate__zoomIn'>View case study <i class="fas fa-arrow-right"></i></p>
             <p class="me-5">SKIP</p>
         </div></div>`;
         img51.classList.remove("animate__fadeInDown")
